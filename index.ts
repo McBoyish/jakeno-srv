7943cc595c168e4b516c7a7fe5b66f5a9eb756c2
jake <jake@eventdesk.co>
2022-02-13 13:57:04 -0500

    whole revamp

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
 require('dotenv').config();
-import { Collection, MongoClient } from 'mongodb';
-import cuid from 'cuid';
-import { Socket } from 'socket.io';
-import { Room, Message } from './types';
-const io = require('socket.io')(process.env.PORT || 4000, {
+import { Collection, MongoClient, ObjectId } from 'mongodb';
+import { Server, Socket } from 'socket.io';
+import { Room, Message, User, InputMessage, MessageField } from './types';
+const io: Server = require('socket.io')(process.env.PORT || 4000, {
 	cors: {
 		origin: [process.env.ORIGIN, 'http://localhost:3000'],
 	},
@@ -17,37 +16,91 @@ async function run() {
 	const db = client.db('randomstranger');
 
 	io.on('connection', (socket: Socket) => {
-		socket.on('get-room-id', async (roomName: string, callback: any) => {
-			const rooms: Collection<Room> = db.collection('rooms');
-			const room = await rooms.findOne({ roomName });
-			if (room) {
-				callback(room.roomId);
+		socket.on(
+			'join-room-request',
+			async (
+				userName: string,
+				roomName: string,
+				callback: (userId: string, roomId: string) => void
+			) => {
+				const userCollection: Collection<User> = db.collection('users');
+				const roomCollection: Collection<Room> = db.collection('rooms');
+				const user = await userCollection.findOne({ name: userName });
+				const room = await roomCollection.findOne({ name: roomName });
+				let userId = '';
+				let roomId = '';
+				if (user) userId = user._id;
+				else {
+					const res = await userCollection.insertOne({
+						_id: new ObjectId().toString(),
+						name: userName,
+					});
+					userId = res.insertedId;
+				}
+				if (room) roomId = room._id;
+				else {
+					const res = await roomCollection.insertOne({
+						_id: new ObjectId().toString(),
+						name: roomName,
+					});
+					roomId = res.insertedId;
+				}
+				callback(userId, roomId);
+			}
+		);
+		socket.on('join-room', async (userId: string, roomId: string) => {
+			const userCollection: Collection<User> = db.collection('users');
+			const roomCollection: Collection<Room> = db.collection('rooms');
+			const user = await userCollection.findOne({ _id: userId });
+			const room = await roomCollection.findOne({ _id: roomId });
+			if (!user || !room || !userId || !roomId) {
+				// user entered room using query parameters (not allowed)
+				socket.emit('join-room-error');
 				return;
 			}
-			const data: Room = {
-				roomId: cuid().toString(),
-				roomName: roomName,
-			};
-			await rooms.insertOne(data);
-			callback(data.roomId);
-		});
-		socket.on('join-room', async (roomId: string) => {
 			socket.join(roomId);
-			const allMessages: Collection<Message> = db.collection('messages');
-			const messages = await allMessages.find({ roomId }).toArray();
-			socket.emit('join', messages.reverse());
+			const messageCollection: Collection<Message> = db.collection('messages');
+			// can be refactored in the future
+			const messages = await messageCollection.find({ roomId }).toArray();
+			const users = await userCollection.find({}).toArray();
+			const res: MessageField[] = messages.map(message => {
+				const user = users.find(user => user._id === message.userId);
+				return {
+					date: message.date,
+					content: message.content,
+					userName: user.name,
+				};
+			});
+			socket.emit('join-room-success', res);
 		});
 		socket.on('leave-room', (roomId: string) => {
 			socket.leave(roomId);
 		});
-		socket.on('message', async (roomId: string, message: Message) => {
-			const messages: Collection<Message> = db.collection('messages');
-			await messages.insertOne({
-				...message,
-				roomId,
-			});
-			socket.to(roomId).emit('message', message);
-		});
+		socket.on(
+			'message',
+			async (message: InputMessage, callback: (res: MessageField) => void) => {
+				const messageCollection: Collection<Message> =
+					db.collection('messages');
+				const userCollection: Collection<User> = db.collection('users');
+				const data = {
+					_id: new ObjectId().toString(),
+					date: new Date().toISOString(),
+					userId: message.userId,
+					roomId: message.roomId,
+					content: message.content,
+				};
+				await messageCollection.insertOne(data);
+				const user = await userCollection.findOne({ _id: data.userId });
+				const res: MessageField = {
+					date: data.date,
+					content: data.content,
+					userName: user.name,
+				};
+				console.log(res);
+				socket.to(message.roomId).emit('message', res);
+				callback(res);
+			}
+		);
 	});
 }
 
--- types.ts
+++ types.ts
@@ -1,10 +1,27 @@
 export interface Room {
-	roomId: string;
-	roomName: string;
+	_id: string;
+	name: string;
 }
-
 export interface Message {
-	user: string;
-	content: string;
+	_id: string;
+	date: string;
+	userId: string;
 	roomId: string;
+	content: string;
+}
+export interface User {
+	_id: string;
+	name: string;
+}
+// emit to client
+export interface MessageField {
+	date: string;
+	content: string;
+	userName: string;
+}
+// emit from client
+export interface InputMessage {
+	userId: string;
+	roomId: string;
+	content: string;
 }
