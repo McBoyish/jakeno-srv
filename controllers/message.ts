57a37c1557d43c162ac24cbc0ee80db367593799
mcboyish <jakelsh@hotmail.com>
2022-08-13 22:39:45 -0400

    sort messages

--- controllers/message.ts
+++ controllers/message.ts
@@ -9,7 +9,11 @@ messageRouter.post('/', async (req, res, next) => {
 		const { roomName, code } = req.body as { roomName: string; code: string };
 		const room = await findOne<Room>('rooms', { name: roomName });
 		if (room.code !== code) res.json(null);
-		const messages = await find<Message>('messages', { roomId: room._id });
+		const messages = await find<Message>(
+			'messages',
+			{ roomId: room._id },
+			{ sort: { _id: -1 } }
+		);
 		res.json(messages);
 	} catch (e) {
 		next(new Error('unknown-error'));
