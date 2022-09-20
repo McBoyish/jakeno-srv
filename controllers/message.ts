5d146a67eed14e72c01d9f0c18186a22bf18f7b3
mcboyish <jakelsh@hotmail.com>
2022-09-20 11:06:15 -0400

    added limit

--- controllers/message.ts
+++ controllers/message.ts
@@ -6,10 +6,11 @@ export const messageRouter = express.Router();
 
 messageRouter.post('/', async (req, res, next) => {
 	try {
-		const { roomName, code, cursor } = req.body as {
+		const { roomName, code, cursor, limit } = req.body as {
 			roomName: string;
 			code: string;
 			cursor: string;
+			limit: number;
 		};
 		const room = await collection<Room>('rooms').findOne({ name: roomName });
 		if (room.code !== code) res.json(null);
@@ -19,7 +20,7 @@ messageRouter.post('/', async (req, res, next) => {
 		if (cursor !== '') {
 			findCursor.filter({ _id: { $lt: cursor } });
 		}
-		const messages = await findCursor.sort({ _id: -1 }).limit(50).toArray();
+		const messages = await findCursor.sort({ _id: -1 }).limit(limit).toArray();
 		res.json(messages);
 	} catch (e) {
 		next(new Error('unknown-error'));
